c542b466c2453b8781127b23b7d66ea13b26b274
William Naughton-Gravette <william@willng.me>
2016-01-29 01:04:30 +1100

    added client status column

--- database/migrations/2015_12_16_011053_create_clients_table.php
+++ database/migrations/2015_12_16_011053_create_clients_table.php
@@ -14,10 +14,11 @@ class CreateClientsTable extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('public_id');
+            $table->string('public_id');
             $table->integer('user_id');
             $table->string('name');
             $table->string('industry');
+            $table->string('status');
             $table->string('contact_name');
             $table->string('contact_phone');
             $table->string('contact_email');
