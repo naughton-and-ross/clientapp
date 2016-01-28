4e899f7e693fea4f2526a47b68897a6acea23d48
William Naughton-Gravette <william@willng.me>
2016-01-29 01:04:18 +1100

    make public id writable

--- app/Client.php
+++ app/Client.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    protected $guarded = ['id', 'public_id'];
+
     public function user() {
         return $this->belongsTo('App\User');
     }
