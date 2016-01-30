4502914f95883dd57be868d847133e1584235228
William Naughton-Gravette <william@willng.me>
2016-01-30 19:59:55 +1100

    add dashboard front-end controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Client;
+use App\Invoice;
+use Carbon\Carbon;
+
+class DashboardController extends Controller
+{
+    public function renderDashboard()
+    {
+        $now = Carbon::now()->toDateTimeString();
+        $clients = Client::where('status', 'active')->get();
+
+        foreach ($clients as $client) {
+            $client->client_id = $client->id + 1000;
+        }
+
+        $active_invoices = Invoice::where('is_paid', '0')->get();
+        $overdue_invoices = Invoice::where('is_paid', '0')->where('due_date', '<', $now)->get();
+        $active_total = $active_invoices->sum('amount');
+        $overdue_total = $overdue_invoices->sum('amount');
+
+        return view('app.dashboard', [
+            'clients'       => $clients,
+            'active_total'  => $active_total,
+            'overdue_total' => $overdue_total,
+        ]);
+    }
+}
