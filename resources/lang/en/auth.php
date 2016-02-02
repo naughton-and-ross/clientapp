ad6afba262456234d16e26bacefeab4024d85c21
William Naughton-Gravette <william@willng.me>
2016-02-02 17:49:42 +1100

    added full stop

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'Absolutely not. Try again',
+    'failed' => 'Absolutely not. Try again.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
