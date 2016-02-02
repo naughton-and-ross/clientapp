f9b367ec27fb043fba787cd60aa19f9cb48cfd50
William Naughton-Gravette <william@willng.me>
2016-02-02 17:44:18 +1100

    change login error words

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Absolutely not. Try again',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
