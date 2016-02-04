d3eabb26233274cfc6f4a99e9d16285ffb138ab9
William Naughton-Gravette <william@willng.me>
2016-02-04 17:18:24 +1100

    fetch project activity when viewing project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -63,6 +63,7 @@ class ProjectController extends Controller
         $project = Project::find($id);
         $client = $project->client;
         $project->project_updates = $project->project_updates()->orderBy('created_at', 'desc')->get();
+        $project->project_activity = $project->project_activity()->orderBy('created_at', 'desc')->get();
 
         return view('app.project', [
             'project' => $project,
