b340dcba3ce6840705d43dee56f6d90b14d96cbc
William Naughton-Gravette <william@willng.me>
2016-02-04 17:18:45 +1100

    project activity model + controller

---
+++ app/Http/Controllers/ProjectActivityController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Input;
+use App\ProjectActivity;
+
+class ProjectActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $project_id)
+    {
+        $input = Input::all();
+
+        $activity = new ProjectActivity($input);
+        $activity->user_id = $request->user()->id;
+        $activity->project_id = $project_id;
+        $activity->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/ProjectActivity.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectActivity extends Model
+{
+    protected $fillable= ['activity_title', 'activity_desc', 'activity_icon_code'];
+    
+    public function project()
+    {
+        return $this->belongsTo('App\Project');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
