aef6a5cb594ae2e0e6398d8723987fa61fb37c85
William Naughton-Gravette <william@willng.me>
2016-02-11 02:10:46 +1100

    added unified income and project graph
    plus division by 0 failsafes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,7 +19,7 @@ class DashboardController extends Controller
     public function renderDashboard()
     {
         $now = Carbon::now()->toDateTimeString();
-        $clients = Client::where('status', 'active')->get();
+        $clients = Client::all();
 
         foreach ($clients as $client) {
             $client->client_id = $client->id + 999;
@@ -46,9 +46,21 @@ class DashboardController extends Controller
         $accepted_quotes_total = $accepted_quotes->sum('amount');
 
         $position_difference = $thirty_day_total - $previous_thirty_day_total;
-        $year_difference_percent = $this_year_total / $last_year_total * 100;
-        $financial_year_difference_percent = $this_financial_year_total / $last_financial_year_total * 100;
+
+        if ($this_year_total !== 0 && $last_year_total !== 0) {
+            $year_difference_percent = $this_year_total / $last_year_total * 100;
+        } else {
+            $year_difference_percent = 0;
+        }
+
+        if ($this_financial_year_total !== 0 && $last_financial_year_total !== 0) {
+            $financial_year_difference_percent = $this_financial_year_total / $last_financial_year_total * 100;
+        } else {
+            $financial_year_difference_percent = 0;
+        }
+
         $projected_fy_earnings = $this_financial_year_total + $accepted_quotes_total;
+        $projected_fy_earnings_percent = $projected_fy_earnings / $last_financial_year_total * 100;
 
         foreach ($active_invoices as $invoice) {
             $invoice->client_id = $invoice->client_id + 999;
@@ -82,6 +94,7 @@ class DashboardController extends Controller
             'year_difference_percent'             => $year_difference_percent,
             'financial_year_difference_percent'   => $financial_year_difference_percent,
             'projected_fy_earnings'               => $projected_fy_earnings,
+            'projected_fy_earnings_percent'       => $projected_fy_earnings_percent,
             'active_invoices'                     => $active_invoices
         ]);
     }
