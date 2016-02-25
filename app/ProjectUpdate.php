7b8d901cb998ef1f1d5bd9b54525032bed3c374a
William Naughton-Gravette <william@willng.me>
2016-02-25 18:58:15 +1100

    send SMS for project updates

--- app/ProjectUpdate.php
+++ app/ProjectUpdate.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use SMS;
+
 class ProjectUpdate extends Model
 {
     protected $table = 'project_updates';
@@ -36,6 +38,14 @@ class ProjectUpdate extends Model
                  'user_id' => $project_update->user_id,
                  'activity_type' => 'project_update'
              ]);
+        });
+
+        static::created(function($project_update) {
+            $to_notify = User::all()->except($project_update->user->id);
+
+            foreach ($to_notify as $user) {
+                SMS::send($user->phone_number, 'ClientApp: There\'s new project update for '.$project_update->project->name.' ('.$project_update->project->client->name.'): '.$project_update->comment);
+            }
         });
 
         static::deleting(function($project_update) {
