472fc8289ae578a219984642e8fc81a95c23ab7b
William Naughton-Gravette <william@willng.me>
2016-02-25 14:23:42 +1100

    example SMS notify. job

---
+++ app/Jobs/SendNewClientNotification.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use App\Client;
+use App\User;
+use SMS;
+
+class SendNewClientNotification extends Job implements SelfHandling, ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $users = User::all();
+
+        foreach ($users as $user) {
+            SMS::send($user->phone_number, 'ClientApp: A new client record ($this->client->name) has been added.');
+        }
+    }
+}
