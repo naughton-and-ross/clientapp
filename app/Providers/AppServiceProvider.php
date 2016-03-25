3efebf00de521e7b4603f18f135ba9d3850d84b7
William Naughton-Gravette <william@willng.me>
2016-03-25 12:43:26 +1100

    remove DB code from service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,13 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $commit = DB::table('pushes')->orderBy('id', 'desc')->where('is_deployed', 1)->first();
-        $commit = json_decode($commit->payload, true);
-
-        $commit_diff = DB::table('pushes')->where('is_deployed', 0)->count();
-
-        view()->share('latest_commit', $commit);
-        view()->share('latest_commit_diff', $commit_diff);
+        // $commit = DB::table('pushes')->orderBy('id', 'desc')->where('is_deployed', 1)->first();
+        // $commit = json_decode($commit->payload, true);
+        //
+        // $commit_diff = DB::table('pushes')->where('is_deployed', 0)->count();
+        //
+        // view()->share('latest_commit', $commit);
+        // view()->share('latest_commit_diff', $commit_diff);
     }
 
     /**
